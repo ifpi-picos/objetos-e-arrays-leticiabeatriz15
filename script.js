470b99c90d30f152381f5f1ea2ff67bb9474d567
leticiabeatriz15 <leticiabeatriz1508@gmail.com>
2024-07-13 11:24:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// Crie um objeto para representar um aluno, incluindo propriedades como nome, idade, curso e notas. Popule o objeto com valores e imprima os dados do aluno.
+
+//1
+
+// const aluno = {
+//     nome: 'Juriscleia',
+//     idade: 17,
+//     curso: 'ADS',
+//     notas: '10, 9, 8'
+// }
+
+// console.log(aluno)
+
+// Crie um objeto carro com as propriedades marca, modelo e ano. Em seguida, imprima a marca do carro.
+
+//2
+
+// const carro = {
+//     marca: 'Honda',
+//     modelo: 'civic',
+//     ano: 2022,
+// }
+
+// console.log(carro.marca)
+
+// Crie um objeto livro com as propriedades titulo, autor e anoPublicacao. Imprima o título e o autor do livro.
+
+// 3
+
+// const livro = {
+//     titulo: 'O vendedor de sonhos',
+//     autor: 'fulano',
+//     anoPublicacao: 2007
+// }
+
+// console.log(livro.autor)
+
+// Crie um objeto produto com as propriedades nome, preco e quantidade. Em seguida, calcule e imprima o valor total do estoque (preço * quantidade).
+
+// 4
+
+const produto = {
+    nome: 'Sapato',
+    preco: 40,
+    quantidade: 10
+
+}
+
+const estoque = produto.preco * produto.quantidade
+
+console.log('Total de estoque: ',estoque)
